15d125fbda186bbae0a0e33d0127792af39eda33
Aaron Ishibashi <16326096+tadashibashi@users.noreply.github.com>
2023-08-13 15:26:04 -0700

    Time: 3 ms (41.63%), Space: 6.4 MB (36.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    const char &match(const char &c) {
-        static const char parens[] = {'(', '[', '{'};
-        switch (c) {
-            case ')': return parens[0];
-            case ']': return parens[1];
-            case '}': return parens[2];
-
-            default:
-                throw -1;
-        }
-    }
-
     bool isValid(string s) {
-        stack<char> paren;
+        stack<char> openParen;
 
         for (const char &c : s) {
-            switch (c) {
-                case '[': case '{': case '(': paren.emplace(c); break;
-                case '}': case ']': case ')':
-                    if (paren.empty() || paren.top() != match(c)) return false;
-                    paren.pop();
+            switch(c) {
+                case '[':
+                case '(':
+                case '{':
+                    openParen.emplace(c);
+                    break;
+                case ']':
+                    if (openParen.empty() || openParen.top() != '[') return false;
+                    else openParen.pop();
+                    break;
+                case ')':
+                    if (openParen.empty() || openParen.top() != '(') return false;
+                    else openParen.pop();
+                    break;
+                case '}':
+                    if (openParen.empty() || openParen.top() != '{') return false;
+                    else openParen.pop();
                     break;
             }
         }
 
-        return paren.empty();
+        return openParen.empty();
     }
 };
